d63321304b8e6739f1df1a0191ed2257f26c6b6e
mnovosad1095 <manovos@outlook.com>
2020-05-28 00:59:15 +0300

    added verify for interface

---
+++ python_interface/verify.py
@@ -0,0 +1,31 @@
+import jam_lib
+
+def verify(genome_fname, markers_fname, res_file_name):
+  markers = []
+  
+  with open(genome_fname,"r") as f:
+    genome = f.readline()
+    
+  with open(markers_fname, "r") as f:
+    for line in f.readlines():
+      marker = line.strip().split(",")[1]
+      markers.append(marker)
+  
+  np_res = jam_lib.run(["pseudo88.fasta"], [genome], markers, 1)
+  
+  
+  with open(res_file_name, "r") as f:
+    genome = f.readline()
+    result = genome.split(" ")[1]
+    res = np_res[0][1]
+    for i, ch in enumerate(result):
+      if ch == res[i]:
+        continue
+      else: 
+        raise RuntimeError("Failed")
+  print(np_res)
+    
+  return 0
+
+if __name__ == "__main__":
+    verify("data/bank/pseudo88.fasta", "data/8000markers.csv", "data/out_now.txt")
